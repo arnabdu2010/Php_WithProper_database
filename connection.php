d968faf48fb991f57abcb3af2e61197b5f23190f
Arnab Dutta <55645593+arnabdu2010@users.noreply.github.com>
2020-11-12 23:19:25 +0600

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+
+   $connection= mysqli_connect("localhost","root","","loginapp");
+
+   if($connection){
+       echo "connection ok";
+   }
+   else{
+       echo "connection not ok";
+   }
+
+
+
+
+
+
+?>
